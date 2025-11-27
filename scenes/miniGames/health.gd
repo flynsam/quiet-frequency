679208078f68f6f9d8d1fb2981d2adebebea4f28
flynsam <154352223+flynsam@users.noreply.github.com>
2025-11-27 17:28:35 +0530

    updated

---
+++ scenes/miniGames/health.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+
+
+var _frequency = 1
+var _amplitude = 250
+@export var  game_end = false
+@export var game_start = false
+
+
+
+@export var  state := "error"
+
+func update_color():
+	var colors = {
+		"white": Color("#e0e0e0"),
+		"black": Color("#030303"),
+		"success": Color("#bae3e5"),
+		"error": Color("#ff9f9f")
+		}
+
+	$ColorRect.color = colors[state]
+	$ColorRect.modulate = Color(1, 1, 1, 0.7)
+
+	
+func _process(_delta):
+	update_color()
+	
+	if game_end:
+		state = 'success'
+		_frequency = 1
+		_amplitude = 250
+	elif game_start:
+		queue_redraw()
+		state = 'error'
+	
+	
+	
+func draw_dashed_sine(amplitude: float, frequency: float, phase_shift: float, center_y: float,color,
+				 dash_length: int = 15, gap_length: int = 10):
+	var total_length = $ColorRect.size.x
+	var current_x = 0
+
+	while current_x < total_length:
+		var next_x = min(current_x + dash_length, total_length)
+		var prev_y = amplitude * sin(frequency * current_x * 0.03 + phase_shift) + center_y
+
+		var i = current_x + 1
+		while i <= next_x:
+			var y = amplitude * sin(frequency * i * 0.03 + phase_shift) + center_y
+			draw_line(Vector2(i - 1, prev_y), Vector2(i, y),color,10)
+			prev_y = y
+			i += 1
+			
+		current_x += dash_length + gap_length
+
+
+
+func _draw():
+	var offset = Time.get_ticks_msec() / 300.0 
+	var start_y =  $ColorRect.size.y / 2  
+	#var total_length = parent_size.x
+
+	draw_dashed_sine(-_amplitude,_frequency,offset,start_y,Color8(71, 166, 140))
+	draw_dashed_sine(_amplitude,_frequency,offset,start_y,Color8(306, 255, 0, 102))
+	#draw_dashed_sine(-_amplitude,_frequency,offset,start_y,Color8(138, 154, 91))
+	#draw_dashed_sine(_amplitude,_frequency,offset,start_y,Color8(218, 17, 218))
--- scripts/minigame.gd
+++ scripts/minigame.gd
@@ -3,19 +3,49 @@ extends Node2D
 var freq = preload("res://scenes/miniGames/frequency.tscn")
 var ice = preload("res://scenes/miniGames/icebreaker.tscn")
 var color = preload("res://scenes/miniGames/sortcolors.tscn")
+var start = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pass # Replace with function body.
+
+func game_start() ->void:
 	var f = freq.instantiate()
 	var i = ice.instantiate()
 	var s = color.instantiate()
-	add_child(s)
-	pass # Replace with function body.
-
+	add_child(f)
+	$Health.game_start = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if get_child(2).game_end:
-		$Label.text = "Sucess"
-	else:
-		$Label.text = "Fail"
+	
 	pass
+
+
+func _on_confirm_pressed() -> void:
+	if ! start:
+		game_start()
+		start = true
+	else:
+		get_child(-1)._on_match_pressed()
+	if get_child(-1).game_end:
+		#$Label.text = "Sucess"
+		$Health.game_end = true
+	else:
+		$Health.game_end = false
+	pass # Replace with function body.
+
+
+func _on_power_pressed() -> void:
+	if $CanvasLayer/Screen.visible:
+		$CanvasLayer/Screen.visible = false
+		$Health.state = "white"
+	else:
+		$CanvasLayer/Screen.visible = true
+		$Health.state = "black"
+		$Health/ColorRect.modulate = Color(0, 0, 0, 1)
+	pass # Replace with function body.
+
+
+func _on_info_pressed() -> void:
+	$CanvasLayer/InfoRect.visible = ! $CanvasLayer/InfoRect.visible
+	pass # Replace with function body.
